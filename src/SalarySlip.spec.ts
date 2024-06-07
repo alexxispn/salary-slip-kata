931a0a36b9dd369b78b2a4ba7507788df9364a73
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-06-07 16:30:14 +0100

    🔵 refactor: add parameterization in test

--- src/SalarySlip.spec.ts
+++ src/SalarySlip.spec.ts
@@ -3,7 +3,7 @@ import { SalarySlipGenerator } from "./SalarySlipGenerator.js";
 import { Employee } from "./Employee.js";
 
 describe("SalarySlipGenerator", () => {
-  it('generates a salary with the employee info', () => {
+  it("generates a salary with the employee info", () => {
     const salarySlipGenerator = new SalarySlipGenerator();
     const employee: Employee = {
       name: "John J Doe",
@@ -15,35 +15,25 @@ describe("SalarySlipGenerator", () => {
 
     expect(salarySlip.hasName("John J Doe")).toBe(true);
     expect(salarySlip.hasNationalID("12345")).toBe(true);
-  })
+  });
 
-  it("generates a salary for an employee", () => {
+  it.each([
+    [5000_00, 416_67],
+    [12000_00, 1000_00]
+  ])("calculates monthly gross salary from annual gross salary", (annualGrossSalary, monthlyGrossSalary) => {
     const salarySlipGenerator = new SalarySlipGenerator();
     const employee: Employee = {
       name: "John J Doe",
       nationalId: "12345",
-      annualGrossSalary: 5000_00
+      annualGrossSalary: annualGrossSalary
     };
 
     const salarySlip = salarySlipGenerator.generateFor(employee);
 
-    expect(salarySlip.hasMonthlyGrossSalary(416_67)).toBe(true);
+    expect(salarySlip.hasMonthlyGrossSalary(monthlyGrossSalary)).toBe(true);
   });
 
-  it("generates another salary for another employee", () => {
-    const salarySlipGenerator = new SalarySlipGenerator();
-    const lauraEscanes = {
-      name: "Laura Escanes",
-      nationalId: "90000",
-      annualGrossSalary: 12000_00
-    };
-
-    const salarySlip = salarySlipGenerator.generateFor(lauraEscanes);
-
-    expect(salarySlip.hasMonthlyGrossSalary(416_67)).toBe(false);
-  });
-
-  it('generates a salary that is subject to national insurance contributions', () =>{
+  it("generates a salary that is subject to national insurance contributions", () => {
     const salarySlipGenerator = new SalarySlipGenerator();
     const employee = {
       name: "John J Doe",
@@ -54,5 +44,5 @@ describe("SalarySlipGenerator", () => {
     const salarySlip = salarySlipGenerator.generateFor(employee);
 
     expect(salarySlip.hasNationalInsuranceContribution(10_00)).toBe(true);
-  })
+  });
 });
